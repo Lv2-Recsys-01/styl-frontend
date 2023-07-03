a32c71efa65f1e749886f80dcdce70d9d7c67c01
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-07-03 09:09:47 +0900

    chore: initial commit

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyle from "../styles/globalStyle";
+import Journey from "./Journey";
+
+function App() {
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <Journey />
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ src/pages/Journey/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Journey() {
+    return <div>Journey page</div>;
+}
+
+export default Journey;
